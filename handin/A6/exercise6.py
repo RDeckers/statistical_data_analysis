86d19facba9704e9da3e5cdaa7dd83ba5c788614
Roel Deckers <git@codingcat.nl>
2018-10-21 14:18:59 +0200

    done

--- handin/A6/exercise6.py
+++ handin/A6/exercise6.py
@@ -2,7 +2,11 @@ import ROOT
 import math
 import array
 
-
+#So I was a little bit dumb.
+# It took to long to run imo, so I decided to write a good part in c++ and just throw it in the interpreter :)
+# however, I was deving it locally on my machine which has ROOT7 installed. Turns out ROOT5 is even worse at interpreting
+# c++ than I remember and I don't have the time now to  get it to run on the grid. :\
+# I hope you can grade this on a machine that has ROOT6+.
 ROOT.gInterpreter.ProcessLine('''
 const Int_t palette_size = 512;
 Int_t MyPalette[palette_size];
@@ -344,91 +348,111 @@ def make_cdf(title, H0):
 
 #read the real data
 data = read_real_data()
+#
+# fnc_combined.SetParameter(0,2.1)
+# root_h = ROOT.TH1F("m", ";invariant mass (TeV);entries per bin", 60, 1,3)
+# data.copy_to_root_hist(root_h)
+# root_h.Draw()
+# fnc_combined.Draw("CSAME")
+# fnc_bg.SetLineStyle(2)
+# fnc_bg.Draw("CSAME")
+# canv.Update()
+# canv.SaveAs("results/data.png")
+# canv.Clear()
+#
+# #question a
+# generate_psuedos()
+# ROOT.gStyle.SetOptStat(0)           # Do not show statistics box
+#
+# #question b
+# (H1, H0) = generate_LLR_distribution(1000,hist_range = [-10,14])
+# H1_mean = H1.GetMean()
+# print H1_mean, " = H1 mean" # 2.8868
+# print H0.GetMean(), " = H0 mean" # -2.3788
+# plot_LLR_distribution(H1, H0, "LLR")
+#
+# #question c
+# cdf_H1 = make_cdf("cdf_H1", H1)
+# cdf = make_cdf("cdf", H0)
+# print 1.0 - cdf.Interpolate(H1_mean), " = predicted p-value" #0.0073
+# (H1, H0) = data.ComputeH1H0()
+# print 1.0 - cdf.Interpolate(H1-H0), " = measured p-value" # 0.00264800
+#
+# #part two
 
-#question a
-generate_psuedos()
-ROOT.gStyle.SetOptStat(0)           # Do not show statistics box
+# cdf_H1_dy = make_cdf("cdf_H1_dy", H1)
+# cdf_dy.SetLineColor(ROOT.kRed)
+# cdf_H1_dy.SetLineColor(ROOT.kRed)
+# canv.Divide(2,1)
+# canv.cd(1)
+# cdf.Draw("HISTL")
+# cdf.SetTitle("CDF H0;H0;Cumulative distribution")
+# cdf_dy.Draw("HISTL SAME")
+# canv.cd(2)
+# cdf_H1.SetTitle("CDF H1;H1;Cumulative distribution")
+# cdf_H1.Draw("HISTL")
+# cdf_H1_dy.Draw("HISTL SAME")
+# canv.Update()
+# canv.SaveAs("results/cdfs_compared.png")
+# canv.Clear()
 
-#question b
-(H1, H0) = generate_LLR_distribution(1000,hist_range = [-10,14])
-H1_mean = H1.GetMean()
-print H1_mean, " = H1 mean" # 2.8868
-print H0.GetMean(), " = H0 mean" # -2.3788
-plot_LLR_distribution(H1, H0, "LLR")
-
-#question c
-cdf_H1 = make_cdf("cdf_H1", H1)
-cdf = make_cdf("cdf", H0)
-print 1.0 - cdf.Interpolate(H1_mean), " = predicted p-value" #0.0073
-(H1, H0) = data.ComputeH1H0()
-print 1.0 - cdf.Interpolate(H1-H0), " = measured p-value" # 0.00264800
-
-#part two
-(H1, H0) = generate_LLR_distribution(1000, 2.1, True)
-
+(H1, H0) = generate_LLR_distribution(50000, 2.1, True)
 H1_mean = H1.GetMean()
 print H1_mean, " = H1 mean" # 2.8868
 print H0.GetMean(), " = H0 mean" # -2.3788
 plot_LLR_distribution(H1, H0, "LLR_dynamic")
 
+H0_fit = data.ComputeH0()
+(H1_fit, m_fit) = data.ComputeH1VariableMass(1.2, 2.8, 0.01)
+(H1, H0) = generate_LLR_distribution(50000, m_fit, True)
+
+H1_mean = H1.GetMean()
+print H1_mean, " = H1 mean" # 2.8868
+print H0.GetMean(), " = H0 mean" # -2.3788
+plot_LLR_distribution(H1, H0, "LLR_dynamic_fit")
+
 cdf_dy = make_cdf("cdf_dynamic", H0)
-cdf_H1_dy = make_cdf("cdf_H1_dy", H1)
-cdf_dy.SetLineColor(ROOT.kRed)
-cdf_H1_dy.SetLineColor(ROOT.kRed)
-canv.Divide(2,1)
-canv.cd(1)
-cdf.Draw("HISTL")
-cdf.SetTitle("CDF H0;H0;Cumulative distribution")
-cdf_dy.Draw("HISTL SAME")
-canv.cd(2)
-cdf_H1.SetTitle("CDF H1;H1;Cumulative distribution")
-cdf_H1.Draw("HISTL")
-cdf_H1_dy.Draw("HISTL SAME")
-canv.Update()
-canv.SaveAs("results/cdfs_compared.png")
-canv.Clear()
 print 1.0 - cdf_dy.Interpolate(H1_mean), " = predicted dynamic p-value" #0.0073
-H0 = data.ComputeH0()
-(H1, m_fit) = data.ComputeH1VariableMass(1.2, 2.8, 0.1)
-print 1.0 - cdf_dy.Interpolate(H1-H0), " = measured dynamic p-value" # 0.00264800
+print 1.0 - cdf_dy.Interpolate(H1_fit-H0_fit), " = measured dynamic p-value" # 0.00264800
 print m_fit, " = m fit"
-
-fnc_combined.SetParameter(0,m_fit)
-root_h = ROOT.TH1F("fitted m", "Fit result;invariant mass (TeV);entries per bin", 60, 1,3)
-data.copy_to_root_hist(root_h)
-root_h.Draw()
-fnc_combined.Draw("CSAME")
-fnc_bg.SetLineStyle(2)
-fnc_bg.Draw("CSAME")
-canv.Update()
-canv.SaveAs("results/fit.png")
-canv.Clear()
-
-nM = 10
-M = array.array('d',[1.2 + 1.6*m_i/nM for m_i in range(nM + 1)])
-P = array.array('d',[0 for _ in M])
-n_entries = 500
-H1_2d = ROOT.TH2F('2dH1', 'LLR distribution of H1 vs particle mass M_{z};\lambda;M_{z};number of entries', 2000, -2, 16, nM, 1.2, 2.8)
-for i,m in enumerate(M):
-    H1 = generate_LLR_H1(n_entries, m, True, 2000, [-2,16])
-    for j in range(n_entries):
-        H1_2d.SetBinContent(j+1, i+1, H1.GetBinContent(j+1))
-    H1_mean = H1.GetMean()
-    p =  1.0 - cdf_dy.Interpolate(H1_mean)
-    P[i] = p
-    print m, H1_mean, p
-
-H1_2d.RebinX(20)
-H1_2d.Draw("COLZ")
-canv.Update()
-canv.SaveAs("results/2dH1H0.png")
-canv.Clear()
-
-graph = ROOT.TGraphErrors(len(M),M,P)
-graph.SetMarkerColor(ROOT.kRed)
-graph.SetMarkerStyle(21)
-graph.SetTitle("Expected p-value as a function of true M;True Invariant Mass (TeV);Expected p-value")
-graph.Draw("ALP")
-canv.Update()
-canv.SaveAs("results/expected_p_vs_M.png")
-canv.Clear()
+print H1_fit, " = H1 fit"
+#
+# fnc_combined.SetParameter(0,m_fit)
+# root_h = ROOT.TH1F("fitted m", "Fit result;invariant mass (TeV);entries per bin", 60, 1,3)
+# data.copy_to_root_hist(root_h)
+# root_h.Draw()
+# fnc_combined.Draw("CSAME")
+# fnc_bg.SetLineStyle(2)
+# fnc_bg.Draw("CSAME")
+# canv.Update()
+# canv.SaveAs("results/fit.png")
+# canv.Clear()
+#
+# nM = 10
+# M = array.array('d',[1.2 + 1.6*m_i/nM for m_i in range(nM + 1)])
+# P = array.array('d',[0 for _ in M])
+# n_entries = 500
+# H1_2d = ROOT.TH2F('2dH1', 'LLR distribution of H1 vs particle mass M_{z};\lambda;M_{z};number of entries', 2000, -2, 16, nM, 1.2, 2.8)
+# for i,m in enumerate(M):
+#     H1 = generate_LLR_H1(n_entries, m, True, 2000, [-2,16])
+#     for j in range(n_entries):
+#         H1_2d.SetBinContent(j+1, i+1, H1.GetBinContent(j+1))
+#     H1_mean = H1.GetMean()
+#     p =  1.0 - cdf_dy.Interpolate(H1_mean)
+#     P[i] = p
+#     print m, H1_mean, p
+#
+# H1_2d.RebinX(20)
+# H1_2d.Draw("COLZ")
+# canv.Update()
+# canv.SaveAs("results/2dH1H0.png")
+# canv.Clear()
+#
+# graph = ROOT.TGraphErrors(len(M),M,P)
+# graph.SetMarkerColor(ROOT.kRed)
+# graph.SetMarkerStyle(21)
+# graph.SetTitle("Expected p-value as a function of true M;True Invariant Mass (TeV);Expected p-value")
+# graph.Draw("ALP")
+# canv.Update()
+# canv.SaveAs("results/expected_p_vs_M.png")
+# canv.Clear()
